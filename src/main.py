17e903d77052b9a5a5cef661a035ab975b91e275
hyochung301 <koolsam301@gmail.com>
2021-04-14 19:57:28 +0900

    more boiler plate about updating stocks
    April 02

--- src/main.py
+++ src/main.py
@@ -49,15 +49,84 @@ def displayInformation(accountInfo, stockInfos):
         print(" {}:".format(info["symbol"]))                   #getting information of that symbol
         print(" prices:${}".format(info["trades"][0]["p"]))  #actually printing out the stocks' prices  
 
+def createJsonFile():
+    # check if stocks.json exists
+    # if not
+    #   create a stocks.json file
+
+    # TODO: create file if not exist
+
+
+    # once the file is created above (if not already existed), parse the file and return a dict of all the information
+    # if the file has been just created, it will return an empty dict {}
+    return {}
+
+def getStockInformationFromJson(jsonContent, stocks):
+    # this is the wrapper function to initiate updating stock information
+    # iterate (make a for loop) over stocks
+    # for each stock, get the most recent information from jsonContent
+    # if there's no stock information in jsonContent, that means we don't own any of that stock
+    # in that case, buy
+    # if any information is present, get the current price for stock, compare with the previous price saved in jsonContent
+    # do the business logic from there accordingly
+    for stock in stocks:
+        # get information from jsonContent here
+        stockPrice = None # change this with information fround from jsonContent
+        updatedPrice = None # this will the be the current price of the stock
+        numShare = 0
+
+        if stockPrice is None:
+            # we don't have any of this stock, please buy 5 shares (you can change the number of shares)
+            # then update stockPrice
+            buyStockAndReturnPrice(stock)
+            numShare = 5 # we now own the stock
+        else:
+            # we have some of this stock, get the current price and compare it with previous price
+            # then print what actions have been taken here
+            stockInfo = getStockInformation(stock)
+            # TODO: business logic
+            # use buyStockAndReturnPrice and sellStockAndReturnPrice if needed
+
+            numShare = 0 # update this variable with number of shares we own after actions have been taken
+
+        # now that the price is updated, save that info to json file
+        updateJson(stock, updatedPrice, numShare)
+
+def buyStockAndReturnPrice(stock):
+    # we don't have any of this stock, please buy 5 shares (you can change the number of shares)
+    # then update stockPrice
+    # print how much of which stock we bought at which price here
+    # return how much share we bought
+    return 5
+
+def sellStockAndReturnPrice(stock):
+    # For simplicity, sell all the shares we have
+    # we could improve this logic to sell partial shares of what we own, but let's do it later
+    # print how much of which stock we sold at which price here and what the gain is
+    return 0
+
+def updateJson(stock, updatedPrice, numShare):
+    # complete this function to update json file
+    # 2 things need to happen
+    # if numShare is 0, then we don't own any of this stock, remove any information about them in json file
+    # else update the information
+    if numShare > 0:
+        # we bought some of the stock, update the json file
+
+    else:
+        # we sold all the stock, delete any information about the stock from json file
+
+    pass
 
 def main():
-     accountInfo = getAccountInformation()
+
      stocks = ['GOOGL', 'AAPL', 'LYFT', 'ABNB', 'AMZN'] #symbols of stocks used in the program
-     stockInfos = []
-     for stock in stocks:
-        stockInfo = getStockInformation(stock)
-        stockInfos.append(stockInfo)
-     displayInformation(accountInfo, stockInfos)
+     accountInfo = getAccountInformation()
+     jsonContent = createJsonFile()
+     getStockInformationFromJson(jsonContent, stocks)
+
+    # all the logs will be generated inside getSotkcInformationFromJson function for now
+    # displayInformation(accountInfo, stockInfos)
 
 if __name__ == '__main__':
     main()
